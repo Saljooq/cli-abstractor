5d77e6e075c16a8957f025f56c76b0ba9aba85b3
Sal <75107181+Saljooq@users.noreply.github.com>
2022-06-19 16:21:06 -0500

    Some sample code with implementation... still need an ingestion method
    This method allows for two things: One scanning a file and adding to to json store, next extracting a set of all the variable names that go into making a cli. Still need to dynamically scan files and ingest data

---
+++ testInput.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    // printf() displays the string inside quotation
+    printf("{{Hello}}, World! Welcome to the new {{World}}, let's see if this works {{Hello  }}");
+    return 0;
+}
