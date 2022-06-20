1cfe140d025d4158d15d71e02a123815e47adc54
Sal <altaf.saljooq@outlook.com>
2022-06-19 23:33:50 -0500

    basic cli created - might need to add dependencies if input problems persist

--- index-client-module.js
+++ index-client-module.js
@@ -4,20 +4,81 @@ import fs from 'fs'
 import readLine from 'readline'
 
 
+const askForVar = async (varName) => {
 
-const reader = readLine.createInterface({
+
+  const reader = readLine.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  
-  reader.question(`What's your name?        `, name => {
-    console.log(`Hi ${name}!`);
+
+  let res;
+
+
+  process.stdout.write(`What's the ${varName}?        `);
+
+  for await (const line of reader){
+    res = line;
     reader.close();
+  }
+  return res;
+}
+
+
+  
+
+  
+const ingest = async (dataIn, mappings) =>{
+
+  let data = dataIn;
+
+  console.log(mappings)
+  for (let i of mappings){
+
+    const res = await askForVar(i);
+
+    const st = '{{\\s*' + i + '\\s*}}'
+    // console.log(res)
+
+
+    data = data.replace(new RegExp(st, 'g'), res);
+
+  }
+
+  writer('testOutput.c', data);
+}
+
+
+const writer = (fileName, content)=>{
+  fs.writeFile(fileName, content, err => {
+      if (err){
+          console.log(`Error writing to file ${fileName}: ${err}`);
+      }
+  })
+  
+}
+
+const main = async () => {
+
+  let content, mapping;
+  fs.readFile('./store.json', 'utf8', (err, data) => {
+    if (err){
+        console.log(err);
+        return;
+    }
+
+    data = JSON.parse(data);
+
+    console.log(data.mapping);
+
+    
+    content = data.content;
+    mapping = data.mapping;
+
+    ingest(data.content, data.mapping);
   });
 
-  
+ }
 
-  
-const ingest = (data, mappings) =>{
 
-}
+ main();
---
+++ testOutput.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    // printf() displays the string inside quotation
+    printf("slim, World! Welcome to the new shady, let's see if this works slim");
+    return 0;
+}
