6a2a4b216fcd8a821f3327f802fbd05c48f5bf23
Sal <75107181+Saljooq@users.noreply.github.com>
2022-07-12 22:57:59 -0500

    ability to recursively look through the folders and files has been added

--- index-generalised.js
+++ index-generalised.js
@@ -4,6 +4,12 @@ import { promisify } from 'util';
 import readLine from 'readline'
 import data from './store.js'
 
+/**
+ * This should get the content of the file as String from a filenamesfread
+ * 
+ * @param {String} fileName 
+ * @returns 
+ */
 const readFile = async (fileName) => {
 
     try{
@@ -17,17 +23,44 @@ const readFile = async (fileName) => {
     }
 }
 
-const writer = (fileName, content)=>{
+/**
+ * This is a general purpose function to be used for creating the json
+ * and objects to be used for the store
+ */
+const storeJsonCreator = ( content)=>{
     const inputs = regexScan(content);
-    content = JSON.stringify({content : content, 
+    content = {
+        content : content, 
         mapping : inputs
-    }, null, 4);
+    };
+
+    return content;
+}
+
+const writeToStore = (value) => {
+    content = JSON.stringify(value, null, 4);
+
+
+    const data = `const data = ${content}\n\nexport default data;`
+
+    // To be used for all content in sotre
+    const fileName = `./store.js`
+
+    fileWriter(fileName, data);
+}
+
+/**
+ * General purpose function, takes the filename and the string to be written to it
+ * 
+ * @param {String} fileName 
+ * @param {String} content 
+ */
+const fileWriter = (fileName, content) => {
     fs.writeFile(fileName, content, err => {
         if (err){
             console.log(`Error writing to file ${fileName}: ${err}`);
         }
     })
-    
 }
 
 const regexScan = (content) => {
@@ -53,7 +86,12 @@ const regexScan = (content) => {
 
     return [...allInput];
 }
-
+/**
+ * Simplifies the task of asking the user for an input - without libraries
+ * 
+ * @param {String} varName 
+ * @returns 
+ */
 const askForVar = async (varName) => {
 
     const reader = readLine.createInterface({
@@ -78,15 +116,30 @@ const askForVar = async (varName) => {
 
     // We grab all the files on the current path
     // TODO : recursively fetch all the files in the folders in the path
-    let arr = fs.readdirSync('.');
+    let arr = fs.readdirSync('./').map(x => './'+x);
 
     const isFile = fileName => {
         return fs.lstatSync(fileName).isFile();
     };
     
-    const files = arr.filter(x => isFile(x));
+    const files = arr.filter(isFile);
 
     console.log(`files in the current folder are : ${files}`)
+
+    const folders = arr.filter(x => !isFile(x))
+
+    while (folders.length > 0){
+        const folderName = folders.pop()
+
+        console.log(folderName)
+        
+        const inside = fs.readdirSync(folderName).map(x => folderName + "/" + x)
+        console.log(`files in ${folderName} are ${inside.filter(isFile)}`)
+
+        const foldersInside = inside.filter(x => !isFile(x))
+
+        foldersInside && folders.push(...foldersInside)
+    }
 
     // unless specified we put everything in the default flag
 
@@ -98,7 +151,15 @@ const askForVar = async (varName) => {
 
 
 
-    console.log(data)
+    // console.log(data)
+
+    const content_of_c_file = await readFile('./testInput.c')
+    console.log(content_of_c_file);
+
+    const test_for_c_file = storeJsonCreator(content_of_c_file);
+
+    // console.log(test_for_c_file);
+
 
     // const data_json = await readFile('./store.json')
     // console.log(data_json);
